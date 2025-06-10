ffb75cc552c47b52a5600220f106c042d0899779
Robin Godinho <90001949+robingodinho@users.noreply.github.com>
2025-06-10 11:37:25 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: '2m6eh6ih6kudi2mctdoqffut0b', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'us-east-2_2Vov3Ws7R', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
